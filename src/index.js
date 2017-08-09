8e1d11efdedb99dcd3e847809db86fc19de71219
Chickyky <Chickyky@gmail.com>
2017-08-09 10:17:05 +0700

    build ts

---
+++ src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+var api_1 = require("./api");
+exports.Chromeless = api_1["default"];
+var queue_1 = require("./queue");
+exports.Queue = queue_1["default"];
+var local_1 = require("./chrome/local");
+exports.LocalChrome = local_1["default"];
+var util_1 = require("./util");
+exports.version = util_1.version;
+exports.__esModule = true;
+exports["default"] = api_1["default"];
